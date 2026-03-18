0cf9f8a2e4618194f6f7c3eaba00f1a3f9b590fb
Artem Kutsan <artemkutsan@gmail.com>
2026-03-18 13:13:45 +0100

    Initial commit

---
+++ src/pages/page/index.jsx
@@ -0,0 +1,17 @@
+// src/pages/home/index.jsx
+import Meta from '../../components/Meta';
+
+function Page() {
+  return (
+    <>
+      <Meta title="Титул страницы" />
+
+      <div className="container max-w-3xl prose">
+        <h1>Page</h1>
+        <h2>Task</h2>
+      </div>
+    </>
+  );
+}
+
+export default Page;
